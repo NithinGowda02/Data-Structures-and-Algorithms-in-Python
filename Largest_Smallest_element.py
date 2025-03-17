4dbee1d214ae1b97004b5e67b90e3699ad03987e
Nithin K P <nithinkp0212@gmail.com>
2025-03-17 23:04:02 +0530

    Added list method practice of DSA in python

---
+++ Largest_Smallest_element.py
@@ -0,0 +1,7 @@
+def largest(Given_list):
+    print(max(Given_list))
+def smallest(Given_list):
+    print(min(Given_list)) 
+Given_list = [2,34,23,66,77,42,99,12]
+largest(Given_list)
+smallest(Given_list)       

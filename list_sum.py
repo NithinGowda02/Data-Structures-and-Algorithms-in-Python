ccd17f2dcdc7bd5156804a658e759ffaabba2264
Nithin K P <nithinkp0212@gmail.com>
2025-03-17 23:05:24 +0530

    Sum of elements in the given list

---
+++ list_sum.py
@@ -0,0 +1,10 @@
+"""
+Sum of Elements:
+Write a Python program to find the sum of all elements in a list.
+"""
+
+sum = 0
+Number_list = [22,34,33,12,36,56,13,23]
+for i in Number_list:
+    sum += i
+print(f"sum of elements >> {sum}")    
